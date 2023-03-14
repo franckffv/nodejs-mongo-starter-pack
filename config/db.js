424278e30370970f75f8340749a8701caec03d2b
Franck Valentino <71516306+franckffv@users.noreply.github.com>
2023-03-14 15:52:59 +0400

    upgrade: upgrade package to latest

--- config/db.js
+++ config/db.js
@@ -1,6 +1,8 @@
+import colors from 'colors';
 import mongoose from 'mongoose';
 
 const connectDB = async () => {
+  colors.enable();
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MondoBD connected : ${conn.connection.host}`.cyan.underline);
